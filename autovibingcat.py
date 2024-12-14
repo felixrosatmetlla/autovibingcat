fe5d4d333f3af0a189b085f74cb0c4c3a6ddd0f3
Felix Ros <felixrosat@gmail.com>
2024-12-14 10:50:09 +0100

    Restore arguments

--- autovibingcat.py
+++ autovibingcat.py
@@ -138,17 +138,17 @@ def create_vibing_cat(song_title:str, start_time: int):
     
 
 if __name__ == "__main__":
-    # parser = argparse.ArgumentParser(description="Process .wav file to determine the Beats Per Minute.")
-    # parser.add_argument("--title", required=True, help="Song title that want to be used.")
-    # parser.add_argument(
-    #     "--start",
-    #     type=int,
-    #     default=60,
-    #     help="Second of the song where will start the cat video.",
-    # )
+    parser = argparse.ArgumentParser(description="Process .wav file to determine the Beats Per Minute.")
+    parser.add_argument("--title", required=True, help="Song title that want to be used.")
+    parser.add_argument(
+        "--start",
+        type=int,
+        default=60,
+        help="Second of the song where will start the cat video.",
+    )
 
-    # args = parser.parse_args()
+    args = parser.parse_args()
     
-    create_vibing_cat("Strategy - TWICE", 60)
+    create_vibing_cat(args.title, args.start)
 
     print("Done! Your video has been saved in: " + output_path)
