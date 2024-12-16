cad69d2e79915000a11ae8e4776662e3214aab93
Felix Ros <felixrosat@gmail.com>
2024-12-16 18:07:41 +0100

    use async client to upload videos

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ import os
 import re
 import asyncio
 
-from atproto import Client, models
+from atproto import AsyncClient, models
 
 from autovibingcat import create_vibing_cat
 
@@ -12,22 +12,25 @@ FETCH_NOTIFICATIONS_DELAY_SEC = 10
 
 
 async def main() -> None:
-    client = Client()
-    client.login(os.environ['BSKY_VIBINGCAT_HANDLE'], os.environ['BSKY_VIBINGCAT_PASS'])
+    async_client = AsyncClient()
+    await async_client.login(os.environ['BSKY_VIBINGCAT_HANDLE'], os.environ['BSKY_VIBINGCAT_PASS'])
 
     # fetch new notifications
     while True:
         # save the time in UTC when we fetch notifications
-        last_seen_at = client.get_current_time_iso()
+        last_seen_at = async_client.get_current_time_iso()
 
-        response = client.app.bsky.notification.list_notifications()
+        response = await async_client.app.bsky.notification.list_notifications()
         for notification in response.notifications:
             if not notification.is_read and notification.reason == 'mention':
                 # TODO: Check that no response has been generated yet
                 print('Received new request! Start processing it...')
 
-                txt = "@autovibingcat.bsky.social song:\"Strategy - Twice\"; start:60"
-                x = re.search("@autovibingcat\.bsky\.social song:\"(.*)\".*start:(\d+)", txt)
+                posts = await async_client.get_posts([notification.uri])
+                post = posts.posts[0]
+
+                txt = post.record.text
+                x = re.search("@autovibingcat\.bsky\.social song:\"(.*)\".*start:.*(\d+)", txt)
                 songTitle = x[1]
                 startTime = int(x[2])
                 print(f'Request => song:{songTitle}; start:{startTime}')
@@ -38,18 +41,15 @@ async def main() -> None:
                 with open('temp_output/output.mp4', 'rb') as f:
                     vid_data = f.read()
 
-                post = client.get_posts([notification.uri]).posts[0]
                 root_post_ref = models.create_strong_ref(post)
-                client.send_video(text="Here is your video!", video=vid_data, reply_to=models.AppBskyFeedPost.ReplyRef(parent=root_post_ref, root=root_post_ref))
-
-                # example: "Got new notification! Type: like; from: did:plc:hlorqa2iqfooopmyzvb4byaz"
+                await async_client.send_video(text="Here is your video!", video=vid_data, reply_to=models.AppBskyFeedPost.ReplyRef(parent=root_post_ref, root=root_post_ref))
 
         # mark notifications as processed (isRead=True)
-        # client.app.bsky.notification.update_seen({'seen_at': last_seen_at})
+        await async_client.app.bsky.notification.update_seen({'seen_at': last_seen_at})
         print('Successfully process notification. Last seen at:', last_seen_at)
 
         sleep(FETCH_NOTIFICATIONS_DELAY_SEC)
 
 
 if __name__ == '__main__':
-    main()
+    asyncio.get_event_loop().run_until_complete(main())
